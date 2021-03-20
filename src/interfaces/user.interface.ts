11b4ca7b2e0c6037497761926d327ee5084773d3
Crepiks <sayazhan.onlassyn@mail.ru>
2021-03-21 02:42:29 +0600

    feat: store user data in global store

---
+++ src/interfaces/user.interface.ts
@@ -0,0 +1,13 @@
+export default interface User {
+  id: number;
+  phoneNumber: string;
+  firstName: string;
+  lastName: string;
+  birthday: string;
+  description: string;
+  avatar: string;
+  avatarPreview: string;
+  genderId: number;
+  orientationId: number;
+  createdAt: string;
+}
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,12 @@
 import { createStore } from "vuex";
 
 import options from "./modules/options.module";
+import users from "./modules/users.module";
 
 const store = createStore({
   modules: {
     options,
+    users,
   },
 });
 

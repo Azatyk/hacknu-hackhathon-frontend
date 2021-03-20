9152abafd2a77dc4554b68df229bd426c5bc66b8
Azatyk <azatuk2005@mail.ru>
2021-03-20 23:10:13 +0600

    feat: add feed card component

--- src/views/feed.vue
+++ src/views/feed.vue
@@ -1,17 +1,23 @@
 <template>
   <div>
     <cedra-nav />
-    Home
+    <cedra-feed-card
+      imagePath="https://mensby.com/wp-content/uploads/2020/02/pochemu-devushka-ne-cenit-01.jpg"
+      name="Аяулым"
+      :birthday="20"
+    />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import CedraNav from "@/components/common/cedra-nav.component.vue";
+import CedraFeedCard from "@/components/feed/cedra-feed-card.component.vue";
 
 export default defineComponent({
   components: {
     CedraNav,
+    CedraFeedCard,
   },
 });
 </script>
